5081754d7fffe79e726fc9ba3c5c9e677555cd5f
Santiago Caraveo <santiagocbrvg@hotmail.com>
2023-11-29 01:53:15 -0700

    fixes some errors in spelling

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ class TicTacToe:
         
     @staticmethod
     def print_board_nums():
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for i in range(3)]
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')               
 
@@ -67,7 +67,7 @@ def play(game, x_player, o_player, print_game=True):
         if letter == 'O':
             space = o_player.get_move(game)
         else:
-            space = x_player_get_move(game)
+            space = x_player.get_move(game)
     
         if game.make_move(space, letter):
             if print_game:
